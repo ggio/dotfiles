29c2fdef48d73bb195157c6b3da00fcae4a8cb87
ggio <gregoriomirandapsi@gmail.com>
2025-01-30 17:55:36 -0300

    Add some shortcut aliases

--- dot-bashrc.d/aliases.sh
+++ dot-bashrc.d/aliases.sh
@@ -1,11 +1,20 @@
 # Put it in `~/.bashrc.d`
 # To upgrade: `source ~/.bashrc`
+
+# General
+alias cs50='sudo systemctl start docker && code'
+alias docker_start='sudo systemctl start docker'
+
+# AMC
 alias amc='sudo x11docker -I --clipboard --backend=docker --nxagent --home --share $HOME amc_dev auto-multiple-choice'
 alias amcd='sudo systemctl start docker && sudo x11docker -I --clipboard --backend=docker --nxagent --home --share $HOME amc_dev auto-multiple-choice'
-alias cd50="cd ~/Dropbox/PROFISSIONAL/TECH/CS50/devcontainer/"
-alias cs50="sudo systemctl start docker && code"
+
+# Directories shortcuts
 alias docdir='cd ~/Dropbox/ACADEMICO/DOUTORADO/ESCRITAS/'
-alias docker_start='sudo systemctl start docker'
+alias cd50='cd ~/Dropbox/PROFISSIONAL/TECH/CS50/devcontainer/'
+alias techdir='cd ~/Dropbox/UTILIDADES/tech/linux'
+
+# MMHP
 alias mmhp_artigo='cp -a ~/Dropbox/ACADEMICO/MMHP/modelos/artigo/. .'
 alias mmhp_inmemoriam='cp -a ~/Dropbox/ACADEMICO/MMHP/modelos/inmemoriam/. .'
 alias mmhp_pandoc='pandoc texto.docx -o texto.tex'
