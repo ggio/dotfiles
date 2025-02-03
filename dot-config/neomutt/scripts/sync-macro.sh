7fa9204d5ba22abbac74678faa62045ff4407e46
ggio <gregoriomirandapsi@gmail.com>
2025-02-03 19:20:32 -0300

    Neomutt upgrade: adjusting mailboxes and macros

--- dot-config/neomutt/scripts/sync-macro.sh
+++ dot-config/neomutt/scripts/sync-macro.sh
@@ -4,17 +4,15 @@ set -eu
 echo ""
 echo "Sync Macro"
 echo "=========="
-
 echo ""
+
 echo "Running 'msbsync proton'"
 echo "------------------------"
-echo ""
 mbsync proton
 
 echo ""
 echo "Running 'notmuch new'"
 echo "---------------------"
-echo ""
 notmuch new
 
 echo ""
