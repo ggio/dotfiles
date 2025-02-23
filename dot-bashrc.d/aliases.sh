385879a9c33f338d876efe73c23b9e72dfbdd1c7
ggio <gregsmiranda@gmail.com>
2025-02-23 20:53:37 -0300

    Papis: some customizing

--- dot-bashrc.d/aliases.sh
+++ dot-bashrc.d/aliases.sh
@@ -1,9 +1,9 @@
 # General
 alias cs50='sudo systemctl start docker && code'
 alias docker-start='sudo systemctl start docker'
-alias papis-cache='papis cache clear && papis cache update'
+alias papis-cache='papis cache reset'
 alias papis-bib='rm ~/Dropbox/ACADEMICO/DOUTORADO/BIBLIOGRAFIA/biblio.bib && papis export --all --format bibtex --out ~/Dropbox/ACADEMICO/DOUTORADO/BIBLIOGRAFIA/biblio.bib'
-alias papis-cache-geral='papis -l geral cache clear && papis -l geral cache update'
+alias papis-cache-geral='papis -l geral cache reset'
 alias papis-bib-geral='rm ~/Dropbox/BIBLIO/biblio.bib && papis -l geral export --all --format bibtex --out ~/Dropbox/BIBLIO/biblio.bib'
 
 # AMC
@@ -14,7 +14,7 @@ alias amcd='sudo systemctl start docker && sudo x11docker -I --clipboard --backe
 alias cd50='cd ~/Dropbox/PROFISSIONAL/TECH/Estudos/CS50/devcontainer/'
 alias cddoc='cd ~/Dropbox/ACADEMICO/DOUTORADO/'
 alias cddocencia='cd ~/Dropbox/PROFISSIONAL/DOCENCIA'
-alias cdconf='cd ~/.config'
+alias cdconfig='cd ~/.config'
 alias cdtech='cd ~/Dropbox/UTILIDADES/tech/linux'
 alias cdutil='cd ~/Dropbox/UTILIDADES/'
 alias cdsaude='cd ~/Dropbox/UTILIDADES/saude'
