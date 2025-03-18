571cfec6d87616f570ffbb0e053e066746361ee4
ggio <gregoriomirandapsi@gmail.com>
2025-03-18 17:03:15 -0300

    Neovim: suspend papis due some weird error

--- dot-config/nvim/lua/custom/plugins/papis.lua
+++ dot-config/nvim/lua/custom/plugins/papis.lua
@@ -1,30 +1,30 @@
 return {
-    {
-        'jghauser/papis.nvim',
-        dependencies = {
-            'kkharji/sqlite.lua',
-            'MunifTanjim/nui.nvim',
-            'pysan3/pathlib.nvim',
-            'nvim-neotest/nvim-nio',
-            -- if not already installed, you may also want:
-            -- "nvim-telescope/telescope.nvim",
-            -- "hrsh7th/nvim-cmp",
-        },
-        config = function()
-            require('papis').setup {
-                enable_modules = {
-                    ['debug'] = true,
-                },
-                enable_keymaps = true,
-                init_filetypes = { 'latex', 'tex' },
-                cite_formats = {
-                    tex = {
-                        start_str = [[\autocite{]],
-                        end_str = '}',
-                        separator_str = ', ',
-                    },
-                },
-            }
-        end,
-    },
+    -- {
+    --     'jghauser/papis.nvim',
+    --     dependencies = {
+    --         'kkharji/sqlite.lua',
+    --         'MunifTanjim/nui.nvim',
+    --         'pysan3/pathlib.nvim',
+    --         'nvim-neotest/nvim-nio',
+    --         -- if not already installed, you may also want:
+    --         -- "nvim-telescope/telescope.nvim",
+    --         -- "hrsh7th/nvim-cmp",
+    --     },
+    --     config = function()
+    --         require('papis').setup {
+    --             enable_modules = {
+    --                 ['debug'] = true,
+    --             },
+    --             enable_keymaps = true,
+    --             init_filetypes = { 'latex', 'tex' },
+    --             cite_formats = {
+    --                 tex = {
+    --                     start_str = [[\autocite{]],
+    --                     end_str = '}',
+    --                     separator_str = ', ',
+    --                 },
+    --             },
+    --         }
+    --     end,
+    -- },
 }
