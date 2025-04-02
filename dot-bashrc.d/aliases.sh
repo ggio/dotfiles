e2eab63afcd6cd0c1429a19bcba1696b48c692a4
ggio <gregsmiranda@gmail.com>
2025-04-02 11:13:49 -0300

    Bash alias: add proton-reset alias

--- dot-bashrc.d/aliases.sh
+++ dot-bashrc.d/aliases.sh
@@ -1,14 +1,10 @@
-# General
-alias cs50='sudo systemctl start docker && code'
-alias docker-start='sudo systemctl start docker'
-alias papis-cache='papis cache reset'
-alias papis-bib='rm ~/Dropbox/ACADEMICO/DOUTORADO/BIBLIOGRAFIA/biblio.bib && papis export --all --format bibtex --out ~/Dropbox/ACADEMICO/DOUTORADO/BIBLIOGRAFIA/biblio.bib'
-alias papis-cache-geral='papis -l geral cache reset'
-alias papis-bib-geral='rm ~/Dropbox/BIBLIO/biblio.bib && papis -l geral export --all --format bibtex --out ~/Dropbox/BIBLIO/biblio.bib'
-
 # AMC
 alias amc='sudo x11docker -I --clipboard --backend=docker --nxagent --home --share $HOME amc_dev auto-multiple-choice'
 alias amcd='sudo systemctl start docker && sudo x11docker -I --clipboard --backend=docker --nxagent --home --share $HOME amc_dev auto-multiple-choice'
+
+# CS50
+alias cs50='sudo systemctl start docker && code'
+alias docker-start='sudo systemctl start docker'
 
 # Directories shortcuts
 alias cd50='cd ~/Dropbox/PROFISSIONAL/TECH/Estudos/CS50/devcontainer/'
@@ -20,7 +16,17 @@ alias cdutil='cd ~/Dropbox/UTILIDADES/'
 alias cdsaude='cd ~/Dropbox/UTILIDADES/saude'
 alias cdmmhp='cd ~/Dropbox/ACADEMICO/MMHP/'
 
+# Neomutt e afins
+alias proton-reset='rm -r ~/.mail/proton/*'
+
 # MMHP
 alias mmhp-artigo='cp -a ~/Dropbox/ACADEMICO/MMHP/modelos/artigo/. .'
 alias mmhp-inmemoriam='cp -a ~/Dropbox/ACADEMICO/MMHP/modelos/inmemoriam/. .'
 alias mmhp-pandoc='pandoc texto.docx -o texto.tex'
+
+# Papis
+alias papis-cache='papis cache reset'
+alias papis-bib='rm ~/Dropbox/ACADEMICO/DOUTORADO/BIBLIOGRAFIA/biblio.bib && papis export --all --format bibtex --out ~/Dropbox/ACADEMICO/DOUTORADO/BIBLIOGRAFIA/biblio.bib'
+alias papis-cache-geral='papis -l geral cache reset'
+alias papis-bib-geral='rm ~/Dropbox/BIBLIO/biblio.bib && papis -l geral export --all --format bibtex --out ~/Dropbox/BIBLIO/biblio.bib'
+
