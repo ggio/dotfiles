1ee65d7aca082189cfce53b7d867a2d4a713e417
ggio <gregsmiranda@gmail.com>
2025-04-28 14:27:56 -0300

    Add mmhp-dossie alias

--- dot-bashrc.d/aliases.sh
+++ dot-bashrc.d/aliases.sh
@@ -8,25 +8,24 @@ alias docker-start='sudo systemctl start docker'
 
 # Directories shortcuts
 alias cd50='cd ~/Dropbox/PROFISSIONAL/TECH/Estudos/CS50/devcontainer/'
-alias cddoc='cd ~/Dropbox/ACADEMICO/DOUTORADO/'
+alias cddoc='cd ~/Dropbox/ACADEMICO/doutorado/'
 alias cddocencia='cd ~/Dropbox/PROFISSIONAL/DOCENCIA'
 alias cdconfig='cd ~/.config'
 alias cdtech='cd ~/Dropbox/UTILIDADES/tech/linux'
 alias cdutil='cd ~/Dropbox/UTILIDADES/'
 alias cdsaude='cd ~/Dropbox/UTILIDADES/saude'
-alias cdmmhp='cd ~/Dropbox/ACADEMICO/MMHP/'
+alias cdmmhp='cd ~/Dropbox/ACADEMICO/mmhp/'
 
 # Neomutt e afins
 alias proton-reset='rm -r ~/.mail/proton/*'
 
 # MMHP
-alias mmhp-artigo='cp -a ~/Dropbox/ACADEMICO/MMHP/modelos/artigo/. .'
-alias mmhp-inmemoriam='cp -a ~/Dropbox/ACADEMICO/MMHP/modelos/inmemoriam/. .'
+alias mmhp-artigo='cp -a ~/Dropbox/ACADEMICO/mmhp/modelos/artigo/. .'
+alias mmhp-dossie='cp -a ~/Dropbox/ACADEMICO/mmhp/modelos/dossie/. .'
+alias mmhp-inmemoriam='cp -a ~/Dropbox/ACADEMICO/mmhp/modelos/inmemoriam/. .'
 alias mmhp-pandoc='pandoc texto.docx -o texto.tex'
 
 # Papis
-alias papis-cache='papis cache reset'
-alias papis-bib='rm ~/Dropbox/ACADEMICO/DOUTORADO/BIBLIOGRAFIA/biblio.bib && papis export --all --format bibtex --out ~/Dropbox/ACADEMICO/DOUTORADO/BIBLIOGRAFIA/biblio.bib'
-alias papis-cache-geral='papis -l geral cache reset'
+alias papis-bib='rm ~/Dropbox/ACADEMICO/doutorado/bibliografia/biblio.bib && papis export --all --format bibtex --out ~/Dropbox/ACADEMICO/doutorado/bibliografia/biblio.bib'
 alias papis-bib-geral='rm ~/Dropbox/BIBLIO/biblio.bib && papis -l geral export --all --format bibtex --out ~/Dropbox/BIBLIO/biblio.bib'
 
